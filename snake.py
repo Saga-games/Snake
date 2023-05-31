b43552668ee966345c5db4055ca28992136bd81b
Gabriel Medeiros <gabriel.ma20@aluno.ifsc.edu.br>
2023-05-31 12:25:26 +0000

    Commit inicial

---
+++ snake.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+# TELA
+tamanho_tela = (600,600)
+tela = pygame.display.set_mode(tamanho_tela)
+pygame.display.set_caption("Jogo da Cobrinha")
+
+# COBRINHA
+cobrinha_pos = [(300,300)]
+
+while = True:
+  pass
