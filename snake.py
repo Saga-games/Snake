7960bf47dbfb071d397aa559da4123104eb9a182
Gabriel Medeiros <gabriel.ma20@aluno.ifsc.edu.br>
2023-06-01 12:41:15 +0000

    Atualização codigo

--- snake.py
+++ snake.py
@@ -9,5 +9,23 @@ pygame.display.set_caption("Jogo da Cobrinha")
 
 # COBRINHA
 cobrinha_pos = [(300,300)]
+cobrinha_sup = pygame.Surface((10,10))
+cobrinha_sup.fill = ((255, 255, 255))
 
-while = True:
+while True:
+
+  tela.fill((0,0,0))
+
+  # VERIFICAR EVENTOS
+  for event in pygame.event.get():
+
+    # SE CLICAR EM FECHAR, FECHAR
+    if event.type == QUIT:
+      pygame.quit()
+      quit()
+      
+      # DESENHANDO A COBRINHA
+      for posicao in cobrinha_pos: 
+          tela.blit(cobrinha_sup, cobrinha_pos)
+
+      pygame.display.update()
